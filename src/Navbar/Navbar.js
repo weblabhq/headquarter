6fac656e694bf215c168627adb891df810449843
Cristian Guraliuc <guraliucc@gmail.com>
2017-01-03 01:35:55 +0200

    Navbar right menu

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -7,6 +7,22 @@ class Sidebar extends Component {
     return (
       <div className="Navbar">
         <a className="Navbar-logo" href="/">W</a>
+
+        <span className="Navbar-title">Dashboard</span>
+
+        <ul className="Navbar-right">
+          <li>
+            <i className="fa fa-bell-o"></i>
+          </li>
+
+          <li>
+            <i className="fa fa-cog"></i>
+          </li>
+
+          <li>
+            <i className="fa fa-user-circle-o"></i>
+          </li>
+        </ul>
       </div>
     );
   }
